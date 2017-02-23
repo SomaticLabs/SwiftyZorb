ccf549f66ebdddf072cbe4790f5fce459e656e5a
Jacob Rockland <jakerockland@gmail.com>
2017-02-23 13:54:16 -0700

    added basic docs, removed unnecessay extensions

--- MomentSDK/Moment.swift
+++ MomentSDK/Moment.swift
@@ -9,46 +9,93 @@
 import SwiftyBluetooth
 import Alamofire
 
-// MARK: - SDK Functions
+// MARK: - Moment SDK
 
 /**
- For interacting with Moment device
+ Public class for interacting with Moment device. All SDK functions are called from this class.
  */
 final public class Moment {
     
-    // MARK: Bluetooth Connection Methods
+    // MARK: Bluetooth Management Methods
     
     /**
-     Initiates a connection to an advertising Moment device
+     Initiates a connection to an advertising Moment device.
+     
+     Usage Example:
+     
+     ```swift
+     // Attempt connection to an advertising device
+     Moment.connect { result in
+        switch result {
+        case .success:
+            // Connected succeeded
+        case .failure(let error):
+            // An error occurred during connection
+        }
+     }
+     ```
      */
-    func connect(completion: @escaping ConnectPeripheralCallback) {
+    static public func connect(completion: @escaping ConnectPeripheralCallback) {
         bluetoothManager.connect { result in completion(result) }
     }
     
     /**
-     Ends a connection to a connected Moment device
+     Ends a connection to a connected Moment device.
+     
+     Usage Example:
+     
+     ```swift
+     // After calling this method, Moment disconnection will be guaranteed
+     Moment.disconnect()
+     ```
      */
-    func disconnect() {
+    static public func disconnect() {
         bluetoothManager.peripheral?.disconnect { _ in }
     }
     
     /**
-     Resets stored Moment connection
+     Resets stored Moment connection.
+     
+     Usage Example:
+     
+     ```swift
+     // After calling this method, a new Moment connection can be created
+     Moment.resetConnection()
+     ```
      */
-    func resetConnection() {
+    static public func resetConnection() {
         Settings.resetMomentPeripheral()
     }
     
     // MARK: Bluetooth Javascript Methods
     
     /**
-     Writes a given string of Javascript to the connected Moment device
+     Writes a given string of Javascript to the connected Moment device.
+     
+     Usage Example:
+     
+     ```swift
+     // Calling `writeContents` with optimization will require an extra HTTP request, 
+     // but will result in much faster Bluetooth transfer. 
+     //
+     // However for short snippets of Javascript (under 40 bytes), 
+     // such optimization may be unnecessary.
+     let javascript = "Moment.LED.setColor(Moment.Color.ORANGE);"
+     Moment.writeContents(of javascript, optimize: false) { result in
+        switch result {
+        case .success:
+            // Write succeeded
+        case .failure(let error):
+            // An error occurred during write
+        }
+     }
+     ```
      
      - Parameter javascript: The Javascript code to be written
      
      - Parameter optimize: Boolean flag for whether or not the input Javascript should be optimized by the Google closure compiler. Default value is set to `true`, which is the recommended setting for best Bluetooth transfer speed, but can be set to false if alternative behavior is needed (one needs to avoid the extra HTTP request).
      */
-    func write(with javascript: String, optimize: Bool = true, completion: @escaping WriteRequestCallback) {
+    static public func writeContents(of javascript: String, optimize: Bool = true, completion: @escaping WriteRequestCallback) {
         // If optimization flag is set to true, run the Javascript through the Google closure compiler
         if optimize {
             let params: Parameters = [
@@ -72,15 +119,33 @@ final public class Moment {
     }
     
     /**
-     Writes the Javascript code at a given URL to the connected Moment device
+     Writes the Javascript code at a given URL to the connected Moment device.
+     
+     Usage Example:
+     
+     ```swift
+     // Calling `writeScript` with optimization will require an extra HTTP request, 
+     // but will result in much faster Bluetooth transfer. 
+     //
+     // For long JS scripts, optimization is strongly recommended.
+     let url = URL(string: "https://gist.github.com/jakerockland/1de44467c3eaf132a2089b6c88d680b8")!
+     Moment.writeScript(at url) { result in
+        switch result {
+        case .success:
+            // Write succeeded
+        case .failure(let error):
+            // An error occurred during write
+        }
+     }
+     ```
      
      - Parameter script: A URL to the hosted Javascript script to be written
      
      - Parameter optimize: Boolean flag for whether or not the input Javascript should be optimized by the Google closure compiler. Default value is set to `true`, which is the recommended setting for best Bluetooth transfer speed, but can be set to false if alternative behavior is needed (one needs to avoid the extra HTTP request).
      */
-    func write(with script: URL, optimize: Bool = true, completion: @escaping WriteRequestCallback) {
+    static public func writeScript(at url: URL, optimize: Bool = true, completion: @escaping WriteRequestCallback) {
         // Format the script URL to retrieve the raw text version
-        let url = script.appendingPathComponent("raw")
+        let url = url.appendingPathComponent("raw")
         
         // If optimization flag is set to true, run the Javascript through the Google closure compiler
         if optimize {
