2475670d18116a3688031d6cc1dd90bdbea2ce56
Jacob Rockland <jakerockland@gmail.com>
2017-09-11 10:26:05 -0700

    updated haptic timeline UUID

--- SwiftMomentSDK/Identifiers.swift
+++ SwiftMomentSDK/Identifiers.swift
@@ -21,7 +21,7 @@ internal struct Identifiers {
     static let AdvertisedServices = [HapticTimelineServiceUUID]
     
     /// `CBUUID` that advertises the haptic timeline service
-    static let HapticTimelineServiceUUID = CBUUID(string: "A28E9B69-E9B5-4C0A-9217-1C64D051D762")
+    static let HapticTimelineServiceUUID = CBUUID(string: "A28E9217-E9B5-4C0A-9217-1C64D051D762")
     
     /// `CBUUID` for identifying the Nordic UART service
     static let NordicUARTServiceUUID = CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
