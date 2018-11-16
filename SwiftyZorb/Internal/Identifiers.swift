cee9441c768d0fb97edc1b92f9549635fb6b1600
Jacob Rockland <jakerockland@gmail.com>
2018-11-16 16:58:06 -0700

    Added support for multiple hardware versions

--- SwiftyZorb/Internal/Identifiers.swift
+++ SwiftyZorb/Internal/Identifiers.swift
@@ -28,20 +28,25 @@ internal struct Identifiers {
     
     // MARK: - Zorb Specific Bluetooth Service UUIDs
     
-    /// Array of `CBUUID`s representing all services that Zorb device should advertise
-    static let AdvertisedServices = [CBUUID]() // FIXME: Eventually replace with [HapticTimelineServiceUUID]
+    /// Arrays of `CBUUID`s representing all services that Zorb device should advertise
+    static let AdvertisedServices_V1 = [CBUUID]() // FIXME: Eventually replace with [HapticTimelineServiceUUID_V1]
+    static let AdvertisedServices_V2 = [CBUUID]() // FIXME: Eventually replace with [HapticTimelineServiceUUID_V2]
     
-    /// `CBUUID` that advertises the haptic timeline service
-    static let HapticTimelineServiceUUID = CBUUID(string: "A28E9217-E9B5-4C0A-9217-1C64D051D762")
+    /// `CBUUID`s that advertises the haptic timeline service
+    static let HapticTimelineServiceUUID_V1 = CBUUID(string: "A28E9217-E9B5-4C0A-9217-1C64D051D762")
+    static let HapticTimelineServiceUUID_V2 = CBUUID(string: "D6E59217-D763-47FA-A092-C24EE6DF29D3")
     
-    /// `CBUUID` that advertises the setttings characteristic
-    static let SettingsCharacteristicUUID = CBUUID(string: "A28EFC07-E9B5-4C0A-9217-1C64D051D762")
+    /// `CBUUID`s that advertises the setttings characteristic
+    static let SettingsCharacteristicUUID_V1 = CBUUID(string: "A28EFC07-E9B5-4C0A-9217-1C64D051D762")
+    static let SettingsCharacteristicUUID_V2 = CBUUID(string: "D6E5FC07-D763-47FA-A092-C24EE6DF29D3")
     
     /// `CBUUID` for identifying the basic actuator control characteristic
-    static let ActuatorCharacteristicUUID = CBUUID(string: "A28EFC05-E9B5-4C0A-9217-1C64D051D762")
+    static let ActuatorCharacteristicUUID_V1 = CBUUID(string: "A28EFC05-E9B5-4C0A-9217-1C64D051D762")
+    static let ActuatorCharacteristicUUID_V2 = CBUUID(string: "D6E5FC05-D763-47FA-A092-C24EE6DF29D3")
     
     /// `CBUUID` for identifying the pattern trigger characteristic
-    static let PatternTriggerCharacteristicUUID = CBUUID(string: "A28EFC08-E9B5-4C0A-9217-1C64D051D762")
+    static let PatternTriggerCharacteristicUUID_V1 = CBUUID(string: "A28EFC08-E9B5-4C0A-9217-1C64D051D762")
+    static let PatternTriggerCharacteristicUUID_V2 = CBUUID(string: "D6E5FC08-D763-47FA-A092-C24EE6DF29D3")
     
     /// `CBUUID` for identifying the Nordic UART service
     static let NordicUARTServiceUUID = CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
